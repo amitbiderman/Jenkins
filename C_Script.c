53e1d340254df46de0668d361692283a55df5d0a
amitbiderman <58703536+amitbiderman@users.noreply.github.com>
2020-06-08 18:56:47 +0300

    Update C_Script.c

--- C_Script.c
+++ C_Script.c
@@ -1,12 +1,9 @@
-#include <stdlib.h>
-#include "money.h"
-
-START_TEST(test_money_create) {    
-  Money *m;
-  extern Money *money_create(int, char*);
-
-  m = money_create(5, "USD");
-  ck_assert_int_eq(money_amount(m), 5);       
-  ck_assert_str_eq(money_currency(m), "USD"); 
-  money_free(m);
-} END_TEST
+#include <stdio.h>
+int cfileexists(const char * README.md){
+    /* try to open file to read */
+    FILE *file;
+    if (file = fopen(README.md, "r")){
+        fclose(file);
+        return 1;
+    }
+    return 0;
