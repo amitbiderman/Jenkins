f3ccc6eb1f790ac32660f1cb22260d1bde55f7af
amitbiderman <58703536+amitbiderman@users.noreply.github.com>
2020-06-08 18:48:21 +0300

    Update C_Script.c

--- C_Script.c
+++ C_Script.c
@@ -1,6 +1,30 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "money.h"
 
-int main() {
-   printf("Hello, World!");
-   return 0;
+struct Money {
+  int amount;
+  char *currency;
+};
+
+Money *money_create(int amount, char* currency) {
+  Money *m = malloc(sizeof(Money));
+  if (m == NULL) {
+    return NULL;
+  }
+
+  m->amount = amount;
+  m->currency = currency;
+  return m;
+}
+
+int money_amount(Money* m) {
+  return m->amount;
+}
+
+char *money_currency(Money* m) {
+  return m->currency;
+}
+
+void money_free(Money* m) {
+  free(m);
 }
